7b17c7eb4d64ce8981cc9ff670059d07150ec9c3
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-11-03 12:37:32 -0400

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import LoginPage from "../components/login-page";
+import LoginPage from "../../components/login-page";
 
 
 const Home: NextPage = () => {
