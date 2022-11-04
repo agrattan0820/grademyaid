1ea12d6047ffa54cd661065e286b866393f4df79
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-11-04 10:16:55 -0400

    creating login-page component for after login

--- src/components/login-page.tsx
+++ src/components/login-page.tsx
@@ -2,38 +2,10 @@ import router from "next/router";
 
 const LoginPage = () =>{ 
     return (
-        <div className="w-full max-w-xs">
-        <h2>Login Page</h2>
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"  onClick={() => router.push('/forgot-password')}>
-              Forgot Password?
-            </a>
-          </div>
-          <div className="flex items-center justify-between">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" 
-            onClick={() => {
-              //code here to login
-              //loginHandler
-            }
-            }>
-              Sign In
-            </button>
-            <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => router.push('/register-page')}>
-             Sign Up
-            </a>
-          </div>
-        </form>
+      <div>
+        <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login</button>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-500 md:text-5xl lg:text-6xl dark:text-white content-top">Rate My Aid </h1>
       </div>
     );
  };
