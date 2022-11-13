90f25e0ca195dd0b37073a8ab593f2061b208881
GameDog9988 <alex@grattan.me>
2022-11-13 15:30:07 -0500

    fix: session defined for homepage and prettier

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,46 +1,50 @@
-import { useState, useEffect } from 'react'
-import { useUser, useSupabaseClient, Session } from '@supabase/auth-helpers-react'
-import { Database } from '../utils/database.types'
-type Profiles = Database['public']['Tables']['profiles']['Row']
-import Avatar from './Avatar'
+import { useState, useEffect } from "react";
+import {
+  useUser,
+  useSupabaseClient,
+  Session,
+} from "@supabase/auth-helpers-react";
+import { Database } from "../utils/database.types";
+type Profiles = Database["public"]["Tables"]["profiles"]["Row"];
+import Avatar from "./Avatar";
 
 export default function Account({ session }: { session: Session }) {
-  const supabase = useSupabaseClient<Database>()
-  const user = useUser()
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState<Profiles['username']>(null)
-  const [website, setWebsite] = useState<Profiles['website']>(null)
-  const [avatar_url, setAvatarUrl] = useState<Profiles['avatar_url']>(null)
+  const supabase = useSupabaseClient<Database>();
+  const user = useUser();
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState<Profiles["username"]>(null);
+  const [website, setWebsite] = useState<Profiles["website"]>(null);
+  const [avatar_url, setAvatarUrl] = useState<Profiles["avatar_url"]>(null);
 
   useEffect(() => {
-    getProfile()
-  }, [session])
+    getProfile();
+  }, [session]);
 
   async function getProfile() {
     try {
-      setLoading(true)
-      if (!user) throw new Error('No user')
+      setLoading(true);
+      if (!user) throw new Error("No user");
 
-      let { data, error, status } = await supabase
-        .from('profiles')
+      const { data, error, status } = await supabase
+        .from("profiles")
         .select(`username, website, avatar_url`)
-        .eq('id', user.id)
-        .single()
+        .eq("id", user.id)
+        .single();
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
-      alert('Error loading user data!')
-      console.log(error)
+      alert("Error loading user data!");
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -49,13 +53,13 @@ export default function Account({ session }: { session: Session }) {
     website,
     avatar_url,
   }: {
-    username: Profiles['username']
-    website: Profiles['website']
-    avatar_url: Profiles['avatar_url']
+    username: Profiles["username"];
+    website: Profiles["website"];
+    avatar_url: Profiles["avatar_url"];
   }) {
     try {
-      setLoading(true)
-      if (!user) throw new Error('No user')
+      setLoading(true);
+      if (!user) throw new Error("No user");
 
       const updates = {
         id: user.id,
@@ -63,16 +67,16 @@ export default function Account({ session }: { session: Session }) {
         website,
         avatar_url,
         updated_at: new Date().toISOString(),
-      }
+      };
 
-      let { error } = await supabase.from('profiles').upsert(updates)
-      if (error) throw error
-      alert('Profile updated!')
+      const { error } = await supabase.from("profiles").upsert(updates);
+      if (error) throw error;
+      alert("Profile updated!");
     } catch (error) {
-      alert('Error updating the data!')
-      console.log(error)
+      alert("Error updating the data!");
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -87,7 +91,7 @@ export default function Account({ session }: { session: Session }) {
         <input
           id="username"
           type="text"
-          value={username || ''}
+          value={username || ""}
           onChange={(e) => setUsername(e.target.value)}
         />
       </div>
@@ -96,7 +100,7 @@ export default function Account({ session }: { session: Session }) {
         <input
           id="website"
           type="website"
-          value={website || ''}
+          value={website || ""}
           onChange={(e) => setWebsite(e.target.value)}
         />
       </div>
@@ -107,24 +111,27 @@ export default function Account({ session }: { session: Session }) {
           onClick={() => updateProfile({ username, website, avatar_url })}
           disabled={loading}
         >
-          {loading ? 'Loading ...' : 'Update'}
+          {loading ? "Loading ..." : "Update"}
         </button>
       </div>
 
       <div>
-        <button className="button block" onClick={() => supabase.auth.signOut()}>
+        <button
+          className="button block"
+          onClick={() => supabase.auth.signOut()}
+        >
           Sign Out
         </button>
       </div>
       <Avatar
-      uid={username}
-      url={avatar_url}
-      size={150}
-      onUpload={(url) => {
-        setAvatarUrl(url)
-        updateProfile({ username, website, avatar_url: url })
-      }}
-    />
+        uid={username}
+        url={avatar_url}
+        size={150}
+        onUpload={(url) => {
+          setAvatarUrl(url);
+          updateProfile({ username, website, avatar_url: url });
+        }}
+      />
     </div>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Auth, ThemeMinimal, ThemeSupa } from "@supabase/auth-ui-react";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import {
+  useSession,
+  useSupabaseClient,
+  Session,
+} from "@supabase/auth-helpers-react";
 import { fetchSchools } from "../utils/queries";
 import SchoolSearch from "../components/school-search";
 import Account from "../components/Account";
-
 
 const Home: NextPage = () => {
   const session = useSession();
@@ -18,17 +21,28 @@ const Home: NextPage = () => {
   });
 
   async function signOut() {
-    const { error } = await supabase.auth.signOut()
+    const { error } = await supabase.auth.signOut();
   }
 
   // currently have homepage defined in this file, but will move to separate file once we know how to pass supabaseClient to other files
-  const HomePage = () =>{ 
+  const HomePage = ({ session }: { session: Session }) => {
     return (
       <div>
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-500 md:text-5xl lg:text-6xl dark:text-white content-top">Rate My Aid </h1>
-        <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Dashboard</button>
-        <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Settings</button>
-        <button className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={signOut}>Logout</button>
+        <h1 className="content-top mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-500 dark:text-white md:text-5xl lg:text-6xl">
+          Rate My Aid{" "}
+        </h1>
+        <button className="mr-2 mb-2 rounded-full border border-gray-200 bg-white py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+          Dashboard
+        </button>
+        <button className="mr-2 mb-2 rounded-full border border-gray-200 bg-white py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+          Settings
+        </button>
+        <button
+          className="mr-2 mb-2 rounded-full border border-gray-200 bg-white py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+          onClick={signOut}
+        >
+          Logout
+        </button>
         <Account session={session} />
       </div>
     );
@@ -38,12 +52,12 @@ const Home: NextPage = () => {
   const LoginPage = () => {
     return (
       <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      theme="default"
-      providers={["google", "facebook"]} //specify google,facebook sso
-      socialLayout="horizontal"
-    />
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        theme="default"
+        providers={["google", "facebook"]} //specify google,facebook sso
+        socialLayout="horizontal"
+      />
     );
   };
 
@@ -55,13 +69,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
-          {!session ? ( //if not logged in 
-              <LoginPage /> 
-          ) : ( //if logged in
-            <HomePage /> 
-            )}
+          {!session ? ( //if not logged in
+            <LoginPage />
+          ) : (
+            //if logged in
+            <HomePage session={session} />
+          )}
         </div>
         <SchoolSearch></SchoolSearch>
         <pre>{JSON.stringify(schoolQuery, null, 2)}</pre>
@@ -70,8 +84,4 @@ const Home: NextPage = () => {
   );
 };
 
-
-
 export default Home;
-
-
