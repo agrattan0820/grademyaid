6218a4ed18ab2843120e30af37efbad56d46acc4
tylerjglass <tglass622@gmail.com>
2022-11-29 15:05:23 -0500

    added use-saved-grades hook

---
+++ src/utils/hooks/use-saved-grades.ts
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import supabase from "../supabase";
+
+export const getSavedGrades = async (accountId: string) => {
+  const { data, error } = await supabase
+    .from("saved_grades")
+    .select(
+      `
+      grade_id,
+      grade (
+        school_id,
+        grade_num,
+        financial_aid,
+        in_out_loc
+      )`
+    )
+    .eq("account_id", accountId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!data) {
+    throw new Error("Saved grade not found");
+  }
+  return data;
+};
+
+export function useSavedGrades(accountId: string, initialData?: any) {
+  return useQuery(["grade", accountId], () => getSavedGrades(accountId), {
+    initialData: initialData,
+  });
+}
