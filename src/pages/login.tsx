a3e653cc4c074ae792e2f117a70169e9bd04a0b2
GameDog9988 <alex@grattan.me>
2022-12-01 14:51:54 -0500

    test: remove auth specifications

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,15 +4,7 @@ import supabase from "../utils/supabase";
 const LoginPage = () => {
   console.log(supabase);
 
-  return (
-    <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      theme="default"
-      providers={["google", "facebook"]} //specify google,facebook sso
-      socialLayout="horizontal"
-    />
-  );
+  return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
 };
 
 export default LoginPage;
