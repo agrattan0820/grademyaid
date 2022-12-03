07b3644507d7f6326f9a7835528628da678389a3
GameDog9988 <alex@grattan.me>
2022-12-03 17:44:31 -0500

    feat: other cleanup changes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,6 @@ import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import supabase from "../utils/supabase";
 
 const LoginPage = () => {
-  console.log(supabase);
-
   return <Auth supabaseClient={supabase} />;
 };
 
