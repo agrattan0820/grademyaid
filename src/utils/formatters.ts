dc82357527987aa77c729771d0986541055ba2ba
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-12-06 14:37:15 -0500

    updating grade display

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,3 +1,6 @@
-export function numberWithCommas(x: number) {
+export function numberWithCommas(x?: number) {
+  if(x === undefined){
+    return "Data not found"
+  }
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
